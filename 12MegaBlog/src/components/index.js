8a4af60c64f91c23c699729cf6c646dc98642a36
Praful Aswale <prafulcode15@gmail.com>
2024-02-08 07:36:33 +0530

    react hook form

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -5,5 +5,23 @@ import Container from "./Container/Container";
 import LogoutBtn from "./Header/LogoutBtn";
 import Button from "./Button";
 import Input from "./Input";
+import Select from "./Select";
+import PostCard from "./PostCard";
+import Login from "./Login";
+import Signup from "./Signup";
+import Protected from "./AuthLayout";
 
-export { Header, Footer, Container, Logo, LogoutBtn, Button, Input };
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  Button,
+  Input,
+  Select,
+  PostCard,
+  Login,
+  Signup,
+  Protected,
+};
